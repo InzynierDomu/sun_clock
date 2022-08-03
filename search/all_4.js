e0a46a4b3f03638aee707130c1533cbdc2fd4032
InzynierDomu <InzynierDomu@users.noreply.github.com>
2022-08-03 13:23:30 +0000

    deploy: 0980c20cd2ad6b9e3b8b8ce6e0b660022a55d3d4

---
+++ search/all_4.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['horizon_5fsun_0',['horizon_sun',['../_config_8h.html#af1dfd8c04da5741c89d9c8abe674cc89',1,'Config']]]
+];
---
+++ search/all_a.js
@@ -0,0 +1,11 @@
+var searchData=
+[
+  ['serial_5fbaudrate_0',['serial_baudrate',['../_config_8h.html#a0c5ac9f57b257a1e5882446ee75e64eb',1,'Config']]],
+  ['set_5fsky_5frgb_1',['set_sky_rgb',['../main_8cpp.html#ab11231195ed8f66eea4767de796ea6de',1,'main.cpp']]],
+  ['set_5fsun_5frgb_2',['set_sun_rgb',['../main_8cpp.html#aaf627ff1a31669c1bd068b70b62b1829',1,'main.cpp']]],
+  ['setup_3',['setup',['../main_8cpp.html#a4fc01d736fe50cf5b977f755b675f11d',1,'main.cpp']]],
+  ['sin_5ffun_4',['sin_fun',['../main_8cpp.html#ab7bc35f3a63d9c8198485f310a2302ae',1,'main.cpp']]],
+  ['sun_5',['sun',['../main_8cpp.html#a2c320502e60d13869ac70963a5354e62',1,'main.cpp']]],
+  ['sun_5fposition_6',['Sun_position',['../struct_sun__position.html',1,'']]],
+  ['sun_5fposition_7',['sun_position',['../main_8cpp.html#a52609ac0b9d01668f2c13d29c0c2e7e6',1,'main.cpp']]]
+];
